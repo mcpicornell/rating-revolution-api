da92571ebc85310d39f657e45b20a2950375fc04
mcpicornell <marc.castro.picornell@gmail.com>
2025-02-01 13:13:31 +0100

    fix: CORS_ALLOWED_ORIGINS

--- rating_revolution/settings.py
+++ rating_revolution/settings.py
@@ -27,7 +27,8 @@ DEBUG = True
 ALLOWED_HOSTS = ['backend.rating-revolution.mcpicornell.com.es']
 CORS_ALLOWED_ORIGINS = [
     # "http://localhost:3000", # TODO: add real domain once deployed
-    "*"
+    "http://*",
+    "https://*",
 ]
 
 # Application definition
